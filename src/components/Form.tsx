f8d1bb30ee741b5920419a5de4c5f9deaa610b70
Alexander <alexandergonzalezsaavedra@gmail.com>
2024-01-30 11:37:07 -0500

    validacion formulario

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -6,7 +6,10 @@ export const Form = ({ createTodo }: Props) => {
   const [todoText, setTodoText] = useState<string>("");
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
     e.preventDefault();
+    if (todoText === "") return;
     createTodo(todoText);
+    e.currentTarget.reset();
+    setTodoText("");
   };
   const handleChange = (e: React.FormEvent<HTMLInputElement>): void => {
     setTodoText(e.currentTarget.value);
